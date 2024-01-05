226b0db50221221bfbe772cde29e4eba5867334c
AbdulMoiza9257 <abdulmoiza9257@gmail.com>
2024-01-05 13:38:40 -0800

    done

---
+++ app.js
@@ -0,0 +1,430 @@
+
+// CHAPTER 1
+// alert ("first name")
+// alert ("last name")
+// alert ("number")
+// alert ("phone no")
+// alert ("password")
+
+// alert("You're learning JavaScript!");
+// alert("assalam o alaikum");
+// // CHAPTER 2
+
+// var myteam ;
+// myteam=("anus")
+
+// var teamname =("pakistan")
+// alert(teamname);
+
+// var bestman =("charlie")
+// bestman =("anus")
+
+// alert (bestman)
+
+// // CHAPTER 3
+
+// var caseqty ;
+// caseqty = 144;
+
+// var num = 9;
+
+// var sum ;
+// sum = 2+2;
+// alert (sum);
+
+// var merchtotal =100;
+// var shipping =10;
+// var ordertotal = merchtotal+shipping
+// alert (ordertotal);
+
+// var a = 2
+// a= 2+3
+// alert (a)
+
+// // CHAPTER 4
+
+// var  productcoast =3.45 ;
+
+// var nameOfBand;
+
+// var legallyRehman = 10;
+
+// var abdulMoiz =5
+
+// var myName =legal
+// var abdulMoiz =legald
+// // var 12myname =illegal
+// // var 12moiz =illegal
+// //  CHAPTER 5
+// var remainder =10 % 3 ;
+
+// var num = 20 % 6;
+
+// var largeNum = 1000 * 2000;
+// alert (largeNum);
+
+// var a = 5;
+// var b = 3;
+// var c = 5-3
+// alert (c)
+
+// var a =  18
+// var b = 5
+// var result = a % b;
+// alert (result)
+
+// var multiply = 5 * 3
+// alert (multiply)
+
+// // CHAPTER 6
+
+// var a = 1;
+// a= 1+1
+
+// alert (a)
+
+// var a = 100;
+// a= 100-1
+
+// alert (a)
+
+// var x =50;
+// var y = x++;
+
+// alert (y)
+// var x =50;
+// var y = --x;
+
+// alert (y)
+
+// var num1 = 5;
+// var num2  =--num1;
+
+// alert (num2)
+
+// var num1 = 5;
+// var num2  =num1++;
+
+// alert (num2)
+
+// var num = 5 ;
+// num++
+// alert (+ num)
+
+// // CHAPTER 7
+
+// var calculateNum = 4+(2*6);
+// alert (calculateNum)
+// var calculatedNum = (2 + 6) * 6;
+// alert (calculatedNum)
+// var calculatedNum = (2 + 4) * (4 + 2);
+// alert (calculatedNum)
+
+// var calculatedNum = ((2 + 2) * 4) + 5;
+// alert (calculatedNum)
+
+// var coast = (2+2) * (4+10);
+// alert (coast)
+
+// var coast = 2+2 * 4+10 ;
+// alert (coast)
+
+// var coast =4/ (2*4);
+// alert (coast)
+
+// // CHAPTER 8
+// var num = "2" + "2"
+// alert (num)
+
+// var msg = ("hello"+" moiz")
+// alert (msg)
+
+// var msg = ("33"+3)
+// alert (msg)
+
+// var msg = ("pakistan "+"zinddabad")
+// alert (msg)
+
+// var msg =("anus " + 122)
+// alert (msg)
+
+// var a = ("hello")
+// var b = ("boss")
+// var msg = a+b
+// alert (msg)
+
+// // CHAPTER 9
+
+// var a =prompt ("firstname")
+
+// var a = prompt ("Country?", "pakistan")
+
+// var  yourName =prompt ("Enter Your Name")
+
+// var name = prompt ("Name", "abddulmoiz")
+// alert ( name )
+
+// var message = "Enter your name:";
+// var defaultResponse = "John Doe";
+// var userResponse = prompt (message, defaultResponse)
+
+// var a = prompt ("Enter Your Name", "Anas ?");
+// alert (a);
+
+// //  chapter 10
+
+//  var city = ("karachi")
+//  if (city="karachi") {
+//     console.log ("karachi the city of light")
+//  }
+
+// var x = "karachi";
+//  var y = "karachi";
+//  if (x === y){
+//      var z = prompt ("Enter Your CityName")
+//       }
+
+// var zipCode = "10010";
+// if (zipCode == "10010"){
+//     alert ("karachi");
+// }else {
+//     alert ("Please write correct city");
+// }
+
+// var x=1;
+
+// if(x===1){
+//     x=42;
+//     console.log("updated value of x:" + x);
+// }
+
+// chap 11
+
+// 1. Code the first line of an if statement that tests whether one 
+// variable is unequal to another. (Use !)
+// var a = 12;
+// var b = 2;
+// if (a != b ){
+//     alert("meri baat suno")
+// }else {
+//     alert("bhaar me ja")
+// }
+// 2. Code the first line of an if statement that tests whether the 
+// value represented by a variable is greater than or equal to the 
+// value represented by another variable.
+// var a = 85;
+// var b = 85;
+// if (a >= b){
+//     alert ("correct")
+// }else {
+//     alert ("incorrect")
+// }
+// 3. Code an if statement. Test whether a variable is unequal to a 
+// particular number. If so, assign a number to that variable.
+// var a = 89;
+// var b = 78;
+// if (a != b){
+//     alert (a);
+// }else {
+//     alert(b)
+// }
+// 4. Code an if statement that tests whether a number is unequal to 
+// a different number. If the condition is true (it will be), display a 
+// congratulations alert.
+// var a = 89;
+// var b = 78;
+// if (a != b){
+//     alert("Congratulations")
+// }else {
+//     alert("sorry")
+// }
+// 5. Code a prompt asking for your first name.
+// Code an if statement that tests whether the name you entered 
+// is unequal to another name.
+// If the condition is true (it will be), display an alert that says "No 
+// match
+// var a = prompt("Enter Your Name");
+// var b = "Anas";
+// if (a != b){
+//     alert("No matched")
+// }else {
+//     alert("matched")
+// }
+
+
+// chap 12
+
+// 1. Code an if statement that tests whether the value represented by 
+// a variable is greater than or equal to the value represented by 
+// another variable. If so, display an alert. If not, display a different 
+// alert.
+// var a = 89;
+// var b = 78;
+// if (a >= b){
+//     alert("correct")
+// }else{
+//     alert("incorrect")
+// }
+// 2. Write a program using if else and else if statement which take 
+// marks from user. And the program will calculate your percentage 
+// and give you grade A/C to Your percentage. (MARKSHEET)
+// var chem = +prompt("Enter Your marks that u are taken in chem subject? ")
+// var urdu = +prompt("Enter Your marks that u are taken in urdu subject? ")
+// var math = +prompt("Enter Your marks that u are taken in math subject? ")
+// var phy = +prompt("Enter Your marks that u are taken in phy subject? ")
+// var total = 400;
+// var percentage = ((chem + urdu + math + phy)/total * 100);
+// alert (percentage);
+
+// if (percentage >= 80){
+//     alert ("Your grade is A+")
+// }else if (percentage == 79 && percentage >= 70){
+//     alert ("Your grade is A")
+// }else if (percentage == 69 && percentage >= 60){
+//     alert("Your grade is B")
+// }else {
+//     alert ("Your grade is C")
+// }
+
+
+
+// 3. This is the if statement that begins the code.
+// if (a === 10) {
+//  alert("a is 10");
+// }
+// If a isn't 10, display an alert that says The correct value of a is
+// ____
+// Note: Try this by yourself
+
+// var a = +prompt("Enter The Value");
+// if (a === 10){
+//     alert("correct value")
+// }else {
+//     alert (`the value of a is ` +a)
+// }
+
+
+// 4. Prompt the user to enter a city.
+// If the city is Karachi, display an alert acknowledging it is Karachi.
+// If not, check to see if it's Lahore.
+// If it is, display an alert acknowledging it's Lahore.
+// Otherwise, display a different alert
+// var a = prompt("Enter your city name");
+
+
+// onclick = "document.ElementById('demo')= 'Hello JavaScript!'";
+
+// chap 15 {array 1}
+
+// <<<<<<<<<<<<<<<<<___>>>>>>>>>>>>>>>>>>>>></___>
+// question no 1
+// var arr = [];
+// console.log (arr);
+// <<<<<<<<<<<<<<<<<___>>>>>>>>>>>>>>>>>>>>></___>
+
+// <<<<<<<<<<<<<<<<<___>>>>>>>>>>>>>>>>>>>>></___>
+// question no 2
+// var arr = ["anas"];
+// console.log (arr);
+// <<<<<<<<<<<<<<<<<___>>>>>>>>>>>>>>>>>>>>></___>
+
+// <<<<<<<<<<<<<<<<<___>>>>>>>>>>>>>>>>>>>>></___>
+// ye question 3 k answer ko mene apni trf se lamba kya hai seekhne k liye
+// question no 3
+// var arr = ["h","j","k","l"];
+// var x = prompt("Enter the alphabet which you want to know are lies in the data ?")
+// if (arr.includes(x)){
+//     alert (${arr.indexOf(x)} : this number is known as index number, this number placed on this index position. if the answer is -1 that's mean this alphabet is not lies in the array);
+// }else if (!arr.includes(x)){
+//     alert ("not found");
+// }
+// <<<<<<<<<<<<<<<<<___>>>>>>>>>>>>>>>>>>>>></___>
+
+// <<<<<<<<<<<<<<<<<___>>>>>>>>>>>>>>>>>>>>></___>
+// question 3 actual answer
+// var arr = ["h","i","j","k","l"];
+// alert (arr[2]);
+// <<<<<<<<<<<<<<<<<___>>>>>>>>>>>>>>>>>>>>></___>
+
+// <<<<<<<<<<<<<<<<<___>>>>>>>>>>>>>>>>>>>>></___>
+// question 4
+// var alphabet=["h","i","j","k","l","m","n","o",];
+// var total = alphabet.length;
+// console.log (total);
+// <<<<<<<<<<<<<<<<<___>>>>>>>>>>>>>>>>>>>>></___>
+
+// <<<<<<<<<<<<<<<<<___>>>>>>>>>>>>>>>>>>>>></___>
+// var arr = ["a"];
+// arr[1] = "b";
+// alert (arr[1]);
+// console.log (arr);
+// <<<<<<<<<<<<<<<<<___>>>>>>>>>>>>>>>>>>>>></___>
+
+
+// chap 16 {array 2}
+
+// <<<<<<<<<<<<<<<<<___>>>>>>>>>>>>>>>>>>>>></___>
+// question no 1
+// var arr = ["sir arslan"];
+// arr.push = "sir basit";
+// alert (arr.push);
+// <<<<<<<<<<<<<<<<<___>>>>>>>>>>>>>>>>>>>>></___>
+
+// <<<<<<<<<<<<<<<<<___>>>>>>>>>>>>>>>>>>>>></___>
+// question no 2
+// var Alphabet=["h","i","j","k"];
+// var removedElement = Alphabet.pop();
+// alert (removedElement);
+// alert me srf j sow kre ga 
+// console.log (Alphabet);
+// iss me j removed hojaye ga 
+// <<<<<<<<<<<<<<<<<___>>>>>>>>>>>>>>>>>>>>></___>
+
+// <<<<<<<<<<<<<<<<<___>>>>>>>>>>>>>>>>>>>>></___>
+// question no 3
+// var Alphabet=["h","i","j","k"];
+// Alphabet.push (42);
+// alert (Alphabet);
+// <<<<<<<<<<<<<<<<<___>>>>>>>>>>>>>>>>>>>>></___>
+
+// chap 16 {Array 3}
+
+// <<<<<<<<<<<<<<<<<___>>>>>>>>>>>>>>>>>>>>></___>
+// // question no 1
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// var removedElement = sizes.shift();
+// alert (removedElement);
+// console.log (sizes);
+// <<<<<<<<<<<<<<<<<___>>>>>>>>>>>>>>>>>>>>></___>
+
+// <<<<<<<<<<<<<<<<<___>>>>>>>>>>>>>>>>>>>>></___>
+// // // question no 2
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// sizes.unshift("p","q","r");
+// console.log (sizes);
+// <<<<<<<<<<<<<<<<<___>>>>>>>>>>>>>>>>>>>>></___>
+
+// <<<<<<<<<<<<<<<<<___>>>>>>>>>>>>>>>>>>>>></___>
+// // // // question no 3
+// var a = ["anas"];
+// var b = a.unshift("bilal");
+// alert (a[0]);
+// <<<<<<<<<<<<<<<<<___>>>>>>>>>>>>>>>>>>>>></___>
+
+// <<<<<<<<<<<<<<<<<___>>>>>>>>>>>>>>>>>>>>></___>
+// // // // // question no 4
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// var b = sizes.indexOf("XL");
+// console.log (b);
+// sizes.splice(b, 0, "L");
+// console.log (sizes);
+// <<<<<<<<<<<<<<<<<___>>>>>>>>>>>>>>>>>>>>></___>
+
+// Chapter 21 (Changing Case)
+// <<<<<<<<<<<<<<<<<___>>>>>>>>>>>>>>>>>>>>></___>
+// // question no 1
+// var userInput = prompt("Enter Your Name");
+// var allLower = userInput.toLocaleLowerCase();
+// console.log(allLower);
+
+// // question no 2
